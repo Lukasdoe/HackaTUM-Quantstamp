1b8fcae614b9c93b428326f01adf29d3b0c9c0c8
Lukas Döllerer <lukas.doellerer@tum.de>
2021-11-20 00:55:21 +0100

    Withdraw + Deposit

--- bank.sol
+++ bank.sol
@@ -4,12 +4,70 @@ pragma solidity 0.7.0;
 import "bank_interface.sol";
 
 contract Bank is IBank{
+
+    mapping(address => Account) userAccounts;
+    address PriceOracle = "https://goerli.etherscan.io/address/0xc3F639B8a6831ff50aD8113B438E2Ef873845552";
+    address HAK = 0xBefeeD4CB8c6DD190793b1c97B72B60272f3EA6C;
+    address ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    constructor(address _PriceOracle, address _HAK) {
+        PriceOracle = _PriceOracle;
+        HAK = _HAK;
+    }
+
+    function call_oracle(address token) private return (uint256) {
+        return PriceOracle.call(bytes4(keccak256("getVirtualPrice(" + token + ")")));
+    }
+
+    function calc_interest(Account user) private return (uint256) {
+        user.interest += ((block.number - user.lastInterestBlock) * (0.03 / 100)) * user.deposit;
+        user.lastInterestBlock = block.number;
+        return interest;
+    }
+
     function deposit(address token, uint256 amount) override payable external returns (bool) {
+        if (amount == 0) {
+            return false;
+        }
+        if (token == HAK) {
+            amount *= call_oracle(HAK)
+        }
+        else if (token != ETH) {
+            return false;
+        }
+        userAccounts[msg.sender].deposit += amount;
+        calc_interest(userAccounts[msg.sender]);
+
+        emit Deposit(msg.sender, token, amount);
         return true;
     }
     
     function withdraw(address token, uint256 amount) override external returns (uint256) {
-        return 0;
+        if (token == HAK) {
+            amount *= call_oracle(HAK)
+        }
+        else if (token != ETH) {
+            return 0;
+        }
+
+        if (amount > userAccounts[msg.sender].deposit) {
+            return 0;
+        }
+        else if (amount == 0) {
+            amount = userAccounts[msg.sender].deposit;
+        }
+
+        calc_interest(userAccounts[msg.sender]);
+        userAccounts[msg.sender].deposit -= amount;
+
+        amount += userAccounts[msg.sender].interest;
+        userAccounts[msg.sender].interest = 0;
+        
+        if (token == HAK) {
+            amount /= call_oracle(HAK)
+        }
+        emit Withdraw(msg.sender, token, amount);
+        return amount;
     }
     
     function borrow(address token, uint256 amount) override external returns (uint256) {
@@ -29,6 +87,10 @@ contract Bank is IBank{
     }
     
     function getBalance(address token) override view external returns (uint256){
-        return 0;
+        // ether = ETH
+        if (token == ETH) {
+            return userAccounts[msg.sender].deposit;
+        }
+        return userAccounts[msg.sender].deposit / call_oracle(token);
     }
 }
